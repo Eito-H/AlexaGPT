80859b340fbf64dff0153311611308f8402aba83
Eito-H <114639781+Eito-H@users.noreply.github.com>
2023-06-28 17:56:12 +0900

    Update keys.js

--- lambda/keys.js
+++ lambda/keys.js
@@ -1,2 +1,3 @@
 module.exports.OPEN_AI_KEY = 'ここにAPIキーを入れる';
-module.exports.system_message = '関西弁で40文字以内で答えて。'
+module.exports.system_message = '40文字以内で答えて。';
+module.exports.model = 'gpt-3.5-turbo';
